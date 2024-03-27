214abb54b624226ad8ac13ae3ea02e630c31bc2b
cleverever <60687194+cleverever@users.noreply.github.com>
2024-03-26 19:25:50 -0700

    Updated constants and added comments

--- auto_icemaker.ino
+++ auto_icemaker.ino
@@ -5,15 +5,26 @@ const int LOADCELL_DOUT_PIN = 3;
 const int LOADCELL_SCK_PIN = 2;
 const int SERVO_PIN = 5;
 
+//Flip threshold comparison direction.
+//True if adding weight subtracts value.
 const bool INVERT_WEIGHT = true;
+
+//Threshold measurements and servo angles will vary
+//and must be found empirically.
 const long ON_THRESHOLD = 300000;
-const long OFF_THRESHOLD = 100000;
+const long OFF_THRESHOLD = 200000;
 const int SERVO_ON_POS = 140;
 const int SERVO_OFF_POS = 90;
 
-const int LOOP_DELAY = 1000;
-const int SAFETY_COUNT = 5;
-const int SAMPLES = 5;
+//Lower measurement interval to conserve power.
+const int LOOP_DELAY = 250;
+
+//Use multiple samples to debounce system.
+const int SAMPLES = 4;
+
+//Will turn ice maker off if communication is
+//interrupted for SAFETY_COUNT measurements.
+const int SAFETY_COUNT = 4;
 
 HX711 loadcell;
 Servo servo;
